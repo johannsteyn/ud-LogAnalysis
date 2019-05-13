db94707b266ea8e7b321025a00ce4f2cc8695b3b
johannsteyn <47539133+johannsteyn@users.noreply.github.com>
2019-05-13 07:30:00 -0500

    Update Logsanalysis.py

--- Logsanalysis.py
+++ Logsanalysis.py
@@ -1,52 +1,57 @@
 import psycopg2
 
 
-def main():
-   
     conn = psycopg2.connect("dbname=news")
+    c = db.cursor()
+    c.execute(query)
+    rows = c.fetchall()
+    db.close()
+    return rows
 
 
-    cur = conn.cursor()
-
-
-    sql_popular_articles = """
-      SELECT article_view.title, article_view.view
-      FROM article_view
-      ORDER BY article_view.view DESC
-      LIMIT 3;
-    """
-    cur.execute(sql_popular_articles)
-    print("Most popular articles:")
-    for (title, view) in cur.fetchall():
-        print("    {} - {} views".format(title, view))
-    print("-" * 70)
-
-    sql_popular_authors = """
-    SELECT article_view.name, SUM(article_view.view) AS author_view
-    FROM article_view
-    GROUP BY article_view.name
-    ORDER BY author_view DESC;
-    """
-    cur.execute(sql_popular_authors)
-    print("Most popular authors:")
-    for (name, view) in cur.fetchall():
-        print("    {} - {} views".format(name, view))
-    print("-" * 70)
-
-    sql_more_than_one_percent_errors = """
-    SELECT *
-    FROM error_rate
-    WHERE error_rate.percentage > 1
-    ORDER BY error_rate.percentage DESC;
-    """
-    cur.execute(sql_more_than_one_percent_errors)
-    print("Days with more than 1% errors:")
-    for (date, percentage) in cur.fetchall():
-        print("    {} - {}% errors".format(date, percentage))
-    print("-" * 70)
-
-    cur.close()
-    conn.close()
-
-if __name__ == "__main__":
-    main()
+sql_qestion1= """
+select title, num from
+    (select substr(path, 10), count(*) as num from log
+    where path !='/' group by path)
+as hits, articles where substr = slug 
+order by num desc limit 3;
+"""
+results = run_query(query)
+PRINT("Most popular articles:")
+for title, num in result:
+print(sql_question2.format(title,num))
+pass
+sql_qestion2="""
+select name, sum(views) as total_views from
+    (select name, author, title, views from
+        (select substr(path, 10), count(*) as views from log
+            where path !='/' group by path)
+        as hits, articles, authors
+        where substr = slug and author = authors.id
+        order by views desc)
+    as threetables group by name order by total_views desc;
+  """
+ results = run_query(query)
+    PRINT("Most popular article authors")
+    for title, num in result
+    print(sql_question2.format(title, num))
+    pass
+    sql_question3= """
+    select errdate, http_requests, http_404,
+100.0 - http_404 / http_requests as errpct from
+    (select date_trunc('day', time) as reqdate, count(*)
+    as http_requests from log group by reqdate)
+    as requests,
+    (select date_trunc('day', time) as errdate, count(*)
+    as http_404 from log where status = '404 NOT FOUND'
+    group by errdate)
+    as errors
+where reqdate = errdate
+and errors.http_404 > 0.01 - requests.http_requests
+order by errdate desc;
+"""
+results= run_query(query)
+PRINT("More than 1% error days")
+for title, num in result
+print(sql_puestion3.format(titile,num))
+pass
