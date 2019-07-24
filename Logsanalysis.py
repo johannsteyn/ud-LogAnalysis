97a9b3ea338acc954bf8335d3747fefee37fae1b
johannsteyn <47539133+johannsteyn@users.noreply.github.com>
2019-07-24 09:25:06 -0500

    Update Logsanalysis.py

--- Logsanalysis.py
+++ Logsanalysis.py
@@ -7,37 +7,35 @@ rows = c.fetchall()
 db.close()
 
 
-
-sql_qestion1= """
-select title, num from
-    (select substr(path, 10), count(*) as num from log
-    where path !='/' group by path)
-as hits, articles where substr = slug 
-order by num desc limit 3;
-"""
-results = run_query(query)
+def sqlquestion1():
+    sqlq1= """
+    select title, num from
+    as hits, articles where substr = slug 
+    order by num desc limit 3;
+    """
+    results = run_query(query)
 print("Most popular articles:")
 for title, num in result:
     print("title: {}, num: {}", sql_puestion3.format(titile,num))
 pass
-sql_qestion2="""
-select name, sum(views) as total_views from
-    (select name, author, title, views from
-        (select substr(path, 10), count(*) as views from log
-            where path !='/' group by path)
-        as hits, articles, authors
-        where substr = slug and author = authors.id
-        order by views desc)
-    as threetables group by name order by total_views desc;
-  """
-results = run_query(query)
+def sqlquestion2():
+    sqlq2="""
+        select name, sum(views) as total_views from
+        (select name, author, title, views from
+          as hits, articles, authors
+            where substr = slug and author = authors.id
+            order by views desc)
+        as threetables group by name order by total_views desc;
+        """
+    results = run_query(query)
     print("Most popular article authors")
     for title, num in result:
-  print("title: {}, num: {}", sql_puestion3.format(titile,num))
-    pass
-    sql_question3= """
+        print("title: {}, num: {}", sql_puestion3.format(titile,num))
+pass
+def sqlquestion3():
+    sqlq3= """
     select errdate, http_requests, http_404,
-100.0 - http_404 / http_requests as errpct from
+    100.0 - http_404 / http_requests as errpct from
     (select date_trunc('day', time) as reqdate, count(*)
     as http_requests from log group by reqdate)
     as requests,
@@ -45,12 +43,11 @@ results = run_query(query)
     as http_404 from log where status = '404 NOT FOUND'
     group by errdate)
     as errors
-where reqdate = errdate
-and errors.http_404 > 0.01 - requests.http_requests
-order by errdate desc;
-"""
-results= run_query(query)
-PRINT("More than 1% error days")
-for title, num in result:
-print("title: {}, num: {}", sql_puestion3.format(titile,num))
+    and errors.http_404 > 0.01 - requests.http_requests
+    order by errdate desc;
+    """
+    results= run_query(query)
+    print("More than 1% error days")
+    for title, num in result:
+        print("title: {}, num: {}", sql_puestion3.format(titile,num))
 pass
