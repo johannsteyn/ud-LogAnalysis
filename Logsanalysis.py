defb0800faf43fd5e696147a14479233330a73af
johannsteyn <47539133+johannsteyn@users.noreply.github.com>
2019-07-31 18:54:32 -0500

    Update Logsanalysis.py

--- Logsanalysis.py
+++ Logsanalysis.py
@@ -1,10 +1,10 @@
 import psycopg2
-
-conn = psycopg2.connect("dbname=news")
-c = db.cursor()
-c.execute(query)
-rows = c.fetchall()
-db.close()
+def run_query():
+    conn = psycopg2.connect("dbname=news")
+    c = db.cursor()
+    c.execute(query)
+    rows = c.fetchall()
+    db.close()
 
 
 def sqlquestion1():
@@ -14,10 +14,10 @@ def sqlquestion1():
     order by desc;
     """
     results = run_query(query)
-print("Most popular articles:")
-for title, num in result:
-    print("title: {}, num: {}", sql_puestion3.format(titile,num))
-pass
+    print("Most popular articles:")
+    for title, num in result:
+        print("title: {}, num: {}", sql_puestion3.format(titile,num))
+    pass
 def sqlquestion2():
     sqlq2="""
         select name, sum(views) as total_views from
@@ -35,15 +35,15 @@ pass
 def sqlquestion3():
     sqlq3= """
     select time, status
-, count(*) percent
-from log
-where status = '404 NOT FOUND'
-group by time, status
-having count(*) > 0.02
-order by percent desc
+    , count(*) percent
+    from log
+    where status = '404 NOT FOUND'
+    group by time, status
+    having count(*) > 0.02
+    order by percent desc
     """
     results= run_query(query)
     print("More than 1% error days")
     for title, num in result:
         print("title: {}, num: {}", sql_puestion3.format(titile,num))
-pass
+    pass
