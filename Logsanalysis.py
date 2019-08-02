1ec8759111336eab87d6ba2f1af6f9757ebd8c83
johannsteyn <47539133+johannsteyn@users.noreply.github.com>
2019-08-02 11:28:49 -0500

    Update Logsanalysis.py

--- Logsanalysis.py
+++ Logsanalysis.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2
 import psycopg2
+
 def run_query():
     conn = psycopg2.connect("dbname=news")
     c = db.cursor()
@@ -7,34 +8,19 @@ def run_query():
     rows = c.fetchall()
     db.close()
 
-
-def sqlquestion1():
-    sqlq1= """
+sqlq1= """
     select top 3 from articles
     from newsdata
     order by desc;
-    """
-    results = run_query(query)
-    print("Most popular articles:")
-    for title, num in result:
-        print("title: {}, num: {}", sqlquestion1.format(titile,num))
-    pass
-def sqlquestion2():
-    sqlq2="""
+   """           
+sqlq2="""
    select name, count(log.id)
     from views, log, articles
     where log.path=concat('/article/', articles.slug)
     group by name, views
     order by views desc;
-
-        """
-    results = run_query(query)
-    print("Most popular article authors")
-    for title, num in result:
-        print("title: {}, num: {}", sqlquestion2.format(titile,num))
-pass
-def sqlquestion3():
-    sqlq3= """
+  """
+sqlq3= """
     select time, status
     , count(*) percent
     from log
@@ -43,8 +29,24 @@ def sqlquestion3():
     having count(*) > 0.02
     order by percent desc
     """
+def sqlquestion1():
+    results = run_query(query)
+    print("Most popular articles:")
+    for i in result:
+        print(str(i[0] + '---' +` str(i[1]) + 'views')
+    print(" ")
+
+def sqlquestion2():
+    results = run_query(query)
+    print("Most popular article authors")
+    for i in result:
+        print(str(i[0] + '---' +` str(i[1]) + 'views')
+    print(" ")
+
+pass
+def sqlquestion3():
     results= run_query(query)
     print("More than 1% error days")
-    for title, num in result:
-        print("title: {}, num: {}", sqlquestion3.format(titile,num))
-    pass
+    for i in result:
+        print(str(i[0] + '---' +` str(i[1]) + 'views')
+    print(" ")
